5744bac04c3f5ea034571098051232d2cb857a05
Magaiver <magaivermagalhaes.mm@gmail.com>
2025-10-19 11:43:04 -0300

    feat: add layout to dashboard admin

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+import { RoleGuard } from "../components/role-guard";
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function DashboardLayout({ children }: DashboardLayoutProps) {
+  return (
+    <RoleGuard>
+      <div className="flex min-h-screen flex-col">
+        {/* Aqui você pode colocar seu Header se houver */}
+        <main className="flex-1">{children}</main>
+        {/* Footer opcional */}
+      </div>
+    </RoleGuard>
+  );
+}
